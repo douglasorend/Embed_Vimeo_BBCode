a3c6aa4d4a0df05e453313886fc90f966febbcb2
douglasorend <37510276+douglasorend@users.noreply.github.com>
2018-10-15 09:51:50 -0500

    v1.0 - July 12th, 2017
    v1.0 - July 12th, 2017
    o Initial Public Release

---
+++ Subs-BBCode-Vimeo.php
@@ -0,0 +1,106 @@
+<?php
+/**********************************************************************************
+* Subs-BBCode-Vimeo.php
+***********************************************************************************
+* This mod is licensed under the 2-clause BSD License, which can be found here:
+*	http://opensource.org/licenses/BSD-2-Clause
+***********************************************************************************
+* This program is distributed in the hope that it is and will be useful, but      *
+* WITHOUT ANY WARRANTIES; without even any implied warranty of MERCHANTABILITY    *
+* or FITNESS FOR A PARTICULAR PURPOSE.                                            *
+**********************************************************************************/
+if (!defined('SMF')) 
+	die('Hacking attempt...');
+
+function BBCode_Vimeo(&$bbc)
+{
+	// Format: [vimeo width=x height=x frameborder=x]{Vimeo ID}[/vimeo]
+	$bbc[] = array(
+		'tag' => 'vimeo',
+		'type' => 'unparsed_content',
+		'parameters' => array(
+			'width' => array('match' => '(\d+)'),
+			'frameborder' => array('optional' => true, 'match' => '(\d+)'),
+		),
+		'validate' => 'BBCode_Vimeo_Validate',
+		'content' => '{width}|{frameborder}',
+		'disabled_content' => '$1',
+	);
+
+	// Format: [vimeo width=x height=x frameborder=x]{Vimeo ID}[/vimeo]
+	$bbc[] = array(
+		'tag' => 'vimeo',
+		'type' => 'unparsed_content',
+		'parameters' => array(
+			'frameborder' => array('match' => '(\d+)'),
+		),
+		'validate' => 'BBCode_Vimeo_Validate',
+		'content' => '0|0|{frameborder}',
+		'disabled_content' => '$1',
+	);
+
+	// Format: [vimeo]{Vimeo ID}[/vimeo]
+	$bbc[] = array(
+		'tag' => 'vimeo',
+		'type' => 'unparsed_content',
+		'validate' => 'BBCode_Vimeo_Validate',
+		'content' => '0|0|0',
+		'disabled_content' => '$1',
+	);
+}
+
+function BBCode_Vimeo_Button(&$buttons)
+{
+	$buttons[count($buttons) - 1][] = array(
+		'image' => 'vimeo',
+		'code' => 'vimeo',
+		'description' => 'vimeo',
+		'before' => '[vimeo]',
+		'after' => '[/vimeo]',
+	);
+}
+
+function BBCode_Vimeo_Validate(&$tag, &$data, &$disabled)
+{
+	global $txt, $modSettings;
+	
+	if (empty($data))
+		return ($tag['content'] = $txt['vimeo_no_post_id']);
+	$data = strtr(trim($data), array('<br />' => ''));
+	if (strpos($data, 'http://') !== 0 && strpos($data, 'https://') !== 0)
+		$data = 'http://' . $data;
+	$pattern = '#(http|https)://(|(.+?).)vimeo.com/(\d+)#i';
+	if (!preg_match($pattern, $data, $parts))
+		return ($tag['content'] = $txt['vimeo_no_post_id']);
+	$data = $parts[4];
+
+	list($width, $frameborder) = explode('|', $tag['content']);
+	if (empty($width))
+		$width = !empty($modSettings['vimeo_default_width']) ? $modSettings['vimeo_default_width'] : false;
+	$tag['content'] = '<div style="max-width: ' . (empty($width) ? '100%;' : $width . 'px;') . '"><div class="vimeo-wrapper">' .
+		'<iframe src="https://player.vimeo.com/video/' . $data .'?title=0&byline=0&portrait=0&badge=0" scrolling="no" frameborder="' . $frameborder . '"  frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div></div>';
+}
+
+function BBCode_Vimeo_LoadTheme()
+{
+	global $context, $settings;
+	$context['html_headers'] .= '
+	<link rel="stylesheet" type="text/css" href="' . $settings['default_theme_url'] . '/css/BBCode-Vimeo.css" />';
+	$context['allowed_html_tags'][] = '<iframe>';
+}
+
+function BBCode_Vimeo_Settings(&$config_vars)
+{
+	$config_vars[] = array('int', 'vimeo_default_width');
+}
+
+function BBCode_Vimeo_Embed(&$message, &$smileys, &$cache_id, &$parse_tags)
+{
+	$replace = (strpos($cache_id, 'sig') !== false ? '[url]$0[/url]' : '[vimeo]$0[/vimeo]');
+	$pattern = '~(?<=[\s>\.(;\'"]|^)(http|https)://(|(.+?).)vimeo.com/((recording|song)/([\w\d\-\_\%])+/(\d+)_(\d+))\??[/\w\-_\~%@\?;=#}\\\\]?~';
+	$message = preg_replace($pattern, $replace, $message);
+	if (strpos($cache_id, 'sig') !== false)
+		$message = preg_replace('#\[vimeo.*\](.*)\[\/Vimeo\]#i', '[url]$1[/url]', $message);
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,38 @@
+[hr]
+[center][color=red][size=16pt][b]EMBED VIMEO VIDEO BBCODE v1.0[/b][/size][/color]
+[url=http://www.simplemachines.org/community/index.php?action=profile;u=253913][b]By Dougiefresh[/b][/url] -> [url=http://custom.simplemachines.org/mods/index.php?mod=4151]Link to Mod[/url]
+[/center]
+[hr]
+
+[color=blue][b][size=12pt][u]Introduction[/u][/size][/b][/color]
+This modification adds a BBCode to view Vimeo videos.
+
+The BBCode takes this form:
+[code]
+[vimeo width=x]{Vimeo URL}[/vimeo]
+[/code]
+
+[color=blue][b][size=12pt][u]Admin Settings[/u][/size][/b][/color]
+There are a new setting on the [b]Modifications Settings[/b] page:
+o Vimeo default width
+
+[color=blue][b][size=12pt][u]Related Discussion[/u][/size][/b][/color]
+o [url=https://www.simplemachines.org/community/index.php?topic=531060.msg3935225#msg3935225]Request made in Yet Another YouTube BBCode Tag mod[/url]
+
+[color=blue][b][size=12pt][u]Compatibility Notes[/u][/size][/b][/color]
+This mod was tested on SMF 2.0.14, but should work on SMF 2.1 Beta 3, as well as SMF 2.0 and up.  SMF 2.1 Beta 1 & 2, as well as SMF 1.x, is not and will not be supported.
+
+[color=blue][b][size=12pt][u]Changelog[/u][/size][/b][/color]
+The changelog has been removed and can be seen at [url=http://www.xptsp.com/board/index.php?topic=1603.0]XPtsp.com[/url].
+
+[color=blue][b][size=12pt][u]License[/u][/size][/b][/color]
+Copyright (c) 2017, Douglas Orend
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
